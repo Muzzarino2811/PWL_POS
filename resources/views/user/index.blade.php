914dc76e16214873a15a85086bfa10943992bd89
Muzzarino2811 <lordmuzzar2811@gmail.com>
2024-04-05 23:18:56 +0700

    Praktikum 4 Jobsheet 7

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,6 +14,22 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error')}}</div>
             @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label for="" class="col-1 control-label col-form-label">Filter:</label>
+                    </div>
+                    <div class="col-3">
+                        <select name="lavel-id" id="level_id" class="form-control" required>
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id}}">{{ $item->level_nama}}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Jenis Pengguna</small>
+                    </div>
+                </div>
+            </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
@@ -41,6 +57,9 @@
                 "url": "{{ url('user/list') }}",
                 "dataType": "json",
                 "type": "POST"
+                "data": function(d){
+                    d.level_id = $('#level_id').val();
+                }
             },
             columns: [{
                     data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
@@ -73,6 +92,9 @@
                     searchable: false
                 }
             ]
+        });
+        $('#level_id').on('change', function (){
+            dataUser.ajax.reload();
         });
     });
 </script>
